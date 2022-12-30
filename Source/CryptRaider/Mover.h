ae87b4279e20e8bac5c9205c9d9e1678dbcb0d99
tiger <thisisstreamliner@gmail.com>
2022-12-30 23:56:37 +0200

    Add Moving Logic

--- Source/CryptRaider/Mover.h
+++ Source/CryptRaider/Mover.h
@@ -25,4 +25,15 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 		
+private:
+	UPROPERTY(EditAnywhere)
+	FVector MoveOffset;
+
+	UPROPERTY(EditAnywhere)
+	float MoveTime = 4;
+
+	UPROPERTY(EditAnywhere)
+	bool ShouldMove = false;
+
+	FVector OriginalLocation;
 };
